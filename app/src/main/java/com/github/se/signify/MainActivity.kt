f7b4dffb4c6cde7c7362c92365c62aa21e6bbdb5
ziyadbagh <ziyad.baghouri@epfl.ch>
2024-10-13 05:16:15 +0200

    restore the Handlandmark parameters

--- app/src/main/java/com/github/se/signify/MainActivity.kt
+++ app/src/main/java/com/github/se/signify/MainActivity.kt
@@ -27,6 +27,10 @@ class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContent {
+      val context = LocalContext.current
+      val handLandMarkImplementation =
+          HandLandMarkImplementation("hand_landmarker.task", "RFC_model_ir9_opset19.onnx")
+      val handLandMarkViewModel = HandLandMarkViewModel(handLandMarkImplementation, context)
       SignifyTheme { Surface(modifier = Modifier.fillMaxSize()) { SignifyAppPreview() } }
     }
   }
