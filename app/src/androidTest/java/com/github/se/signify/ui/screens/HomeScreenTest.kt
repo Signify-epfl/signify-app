1d72bc6e5253f7e74d35bd8c30bc2b63f257eff8
Thomas Pont <thomasmpont@gmail.com>
2024-10-17 14:58:19 +0200

    Create HomeScreetTest.kt

---
+++ app/src/androidTest/java/com/github/se/signify/ui/screens/HomeScreenTest.kt
@@ -0,0 +1,28 @@
+package com.github.se.signify.ui.screens
+
+import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.onNodeWithTag
+import androidx.compose.ui.test.performClick
+import com.github.se.signify.ui.navigation.NavigationActions
+import com.github.se.signify.ui.navigation.Screen
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.mockito.Mockito.mock
+import org.mockito.Mockito.verify
+import org.mockito.Mockito.`when`
+
+class HomeScreenTest {
+
+  @get:Rule val composeTestRule = createComposeRule()
+
+  private lateinit var navigationActions: NavigationActions
+
+  @Before
+  fun setUp() {
+    navigationActions = mock(NavigationActions::class.java)
+
+    `when`(navigationActions.currentRoute()).thenReturn(Screen.HOME)
+  }
+}
