3de13fde68d760d83346383628aa812ded0f058b
Thomas Pont <thomasmpont@gmail.com>
2024-10-18 00:08:00 +0200

    Optimize unnecessary imports

--- app/src/main/java/com/github/se/signify/ui/screens/Challenge/NewChallengeScreen.kt
+++ app/src/main/java/com/github/se/signify/ui/screens/Challenge/NewChallengeScreen.kt
@@ -3,7 +3,13 @@ package com.github.se.signify.ui.screens.Challenge
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
