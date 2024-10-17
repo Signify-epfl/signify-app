1951f838f897b5b04f63f9197052c48500f2e282
ziyadbagh <ziyad.baghouri@epfl.ch>
2024-10-17 19:37:08 +0200

    update ci file and gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
     alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.ktfmt)
     alias(libs.plugins.gms)
-    alias(libs.plugins.sonar)
+    id("org.sonarqube") version "5.1.0.4882"
 }
 
 
@@ -264,11 +264,10 @@ sonar {
     properties {
         property ("sonar.projectKey", "Signify-epfl_signify-app")
         property ("sonar.organization", "signify-epfl")
-        property ("sonar.projectName", "signify-app")
         property ("sonar.host.url", "https://sonarcloud.io")
-        property ("sonar.androidLint.reportPaths", "${project.layout.buildDirectory.get()}/reports/lint-results-debug.xml")
         property ("sonar.coverage.jacoco.xmlReportPaths", "${project.layout.buildDirectory.get()}/reports/jacoco/jacocoTestReport.xml")
         property ("sonar.junit.ReportPaths", "${project.layout.buildDirectory.get()}/test-results/testDebugUnitTest/")
+        property ("sonar.androidLint.reportPaths", "${project.layout.buildDirectory.get()}/reports/lint-results-debug.xml")
     }
 }
 
