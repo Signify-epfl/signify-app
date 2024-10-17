aeb3863008fed09f8ec27c50bf1d6b0a808f8b2e
Thomas Pont <thomasmpont@gmail.com>
2024-10-18 00:58:11 +0200

    Fix all warnings in FriendsListScreen.kt

--- app/src/main/java/com/github/se/signify/ui/screens/Profile/FriendsListScreen.kt
+++ app/src/main/java/com/github/se/signify/ui/screens/Profile/FriendsListScreen.kt
@@ -1,6 +1,7 @@
 package com.github.se.signify.ui.screens.Profile
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -10,18 +11,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Button
-import androidx.compose.material.ButtonDefaults
-import androidx.compose.material.Card
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.material.TextFieldDefaults
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -30,9 +31,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.github.se.signify.R
 import com.github.se.signify.ui.BackButton
 import com.github.se.signify.ui.navigation.NavigationActions
 
@@ -62,11 +65,7 @@ fun FriendsListScreen(
             Modifier.fillMaxWidth()
                 .border(BorderStroke(2.dp, Color(0xFF05A9FB)), RoundedCornerShape(16.dp)),
         placeholder = { Text("Search with user ID") },
-        colors =
-            TextFieldDefaults.textFieldColors(
-                backgroundColor = Color.Transparent,
-                cursorColor = Color.Black,
-                textColor = Color.Black),
+        colors = TextFieldDefaults.colors(),
         singleLine = true,
         trailingIcon = {
           IconButton(onClick = { onSearchUser(searchQuery) }) {
@@ -88,14 +87,14 @@ fun FriendsListScreen(
         modifier = Modifier.padding(bottom = 8.dp))
 
     Card(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth().background(colorResource(R.color.white)),
         shape = RoundedCornerShape(16.dp),
         border = BorderStroke(2.dp, Color(0xFF05A9FB)),
-        backgroundColor = Color(0xFFDCF1FA)) {
-          Column(modifier = Modifier.padding(16.dp)) {
-            currentFriends.forEach { friend -> FriendItem(friend, onRemoveFriend) }
-          }
-        }
+    ) {
+      Column(modifier = Modifier.padding(16.dp)) {
+        currentFriends.forEach { friend -> FriendItem(friend, onRemoveFriend) }
+      }
+    }
 
     Spacer(modifier = Modifier.height(24.dp))
 
@@ -108,16 +107,16 @@ fun FriendsListScreen(
         modifier = Modifier.padding(bottom = 8.dp))
 
     Card(
-        modifier = Modifier.fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth().background(colorResource(R.color.white)),
         shape = RoundedCornerShape(16.dp),
         border = BorderStroke(2.dp, Color(0xFF05A9FB)),
-        backgroundColor = Color(0xFFDCF1FA)) {
-          Column(modifier = Modifier.padding(16.dp)) {
-            friendRequests.forEach { friendRequest ->
-              FriendRequestItem(friendRequest, onAcceptFriendRequest, onRejectFriendRequest)
-            }
-          }
+    ) {
+      Column(modifier = Modifier.padding(16.dp)) {
+        friendRequests.forEach { friendRequest ->
+          FriendRequestItem(friendRequest, onAcceptFriendRequest, onRejectFriendRequest)
         }
+      }
+    }
   }
 }
 
