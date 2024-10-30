e125c43d8984a1b94ac6e611bf3ba250fb9accc8
ziyadbagh <ziyad.baghouri@epfl.ch>
2024-10-30 03:26:40 +0100

    Add a uid attribute for the quest data class

--- app/src/main/java/com/github/se/signify/model/quest/Quest.kt
+++ app/src/main/java/com/github/se/signify/model/quest/Quest.kt
@@ -1,7 +1,8 @@
 package com.github.se.signify.model.quest
 
 data class Quest(
-    val index: String, // This will contain a unique identifier for the quest - from 1 to 26
+    val index: String, // This will contain the index for the quest - from 1 to 26
     val title: String, // The title of the quest
     val description: String, // The description of the quest
+    val uid : String // This will contain a unique identifier for the document 
 )
