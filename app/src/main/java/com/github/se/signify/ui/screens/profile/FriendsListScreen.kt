b697435d57ab3ed8d925c11dd72a037784522cca
nada_ezziti <nada.ezziti@epfl.ch>
2024-10-31 18:38:29 +0100

    removing useless importations

--- app/src/main/java/com/github/se/signify/ui/screens/profile/FriendsListScreen.kt
+++ app/src/main/java/com/github/se/signify/ui/screens/profile/FriendsListScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Surface
@@ -58,7 +57,6 @@ import kotlinx.coroutines.delay
 
 val currentUserId = FirebaseAuth.getInstance().currentUser?.email?.split("@")?.get(0) ?: "unknown"
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FriendsListScreen(
     navigationActions: NavigationActions,
--- app/src/main/java/com/github/se/signify/ui/screens/profile/SettingsScreen.kt
+++ app/src/main/java/com/github/se/signify/ui/screens/profile/SettingsScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -44,7 +43,6 @@ import com.github.se.signify.model.user.UserViewModel
 import com.github.se.signify.ui.BackButton
 import com.github.se.signify.ui.navigation.NavigationActions
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingsScreen(
     navigationActions: NavigationActions,
