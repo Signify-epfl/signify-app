c4b86a937c4b5e379a557821733ec4cbccddf5dc
ThomasMPont <thomasmpont@gmail.com>
2024-11-01 08:12:09 +0100

    Fix missing import from previous merge with main branch

--- app/src/main/java/com/github/se/signify/ui/screens/home/ASLRecognition.kt
+++ app/src/main/java/com/github/se/signify/ui/screens/home/ASLRecognition.kt
@@ -29,6 +29,7 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material3.Button
