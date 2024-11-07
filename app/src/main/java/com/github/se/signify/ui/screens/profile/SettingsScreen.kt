45a3959591d907dbcce153eadcfdcd2ecb180fc9
nada_ezziti <nada.ezziti@epfl.ch>
2024-11-07 23:29:32 +0100

    new feature : upload a profile picture

--- app/src/main/java/com/github/se/signify/ui/screens/profile/SettingsScreen.kt
+++ app/src/main/java/com/github/se/signify/ui/screens/profile/SettingsScreen.kt
@@ -1,7 +1,12 @@
 package com.github.se.signify.ui.screens.profile
 
+import android.content.Context
+import android.net.Uri
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -9,13 +14,18 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Delete
 import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -34,26 +44,50 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.AsyncImage
 import com.github.se.signify.R
 import com.github.se.signify.model.user.UserViewModel
 import com.github.se.signify.ui.BackButton
 import com.github.se.signify.ui.navigation.NavigationActions
+import java.io.File
+import java.io.FileOutputStream
 
 @Composable
 fun SettingsScreen(
     navigationActions: NavigationActions,
     userViewModel: UserViewModel = viewModel(factory = UserViewModel.Factory)
 ) {
+  val context = LocalContext.current
+
+  LaunchedEffect(Unit) {
+    userViewModel.getUserName(currentUserId)
+    userViewModel.getProfilePictureUrl(currentUserId)
+  }
+
+  val userName = userViewModel.userName.collectAsState()
+  val profilePictureUrl = userViewModel.profilePictureUrl.collectAsState()
 
   var newName by remember { mutableStateOf("") }
-  LaunchedEffect(Unit) { userViewModel.getUserName(currentUserId) }
-  val userName = userViewModel.userName.collectAsState()
+  var selectedImageUrl by remember { mutableStateOf(profilePictureUrl.value) }
+  LaunchedEffect(profilePictureUrl.value) { selectedImageUrl = profilePictureUrl.value }
+
+  val galleryLauncher =
+      rememberLauncherForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+        if (uri != null) {
+          val file = uriToFile(context, uri)
+          if (file != null) {
+            selectedImageUrl = file.absolutePath
+          }
+        }
+      }
 
   Column(
       modifier =
@@ -79,12 +113,30 @@ fun SettingsScreen(
                   horizontalArrangement = Arrangement.Center,
                   verticalAlignment = Alignment.CenterVertically,
               ) {
-                Icon(
-                    imageVector = Icons.Outlined.Edit,
-                    contentDescription = "Edit Profile Picture",
-                    tint = colorResource(R.color.black))
+                Column {
+                  Icon(
+                      modifier = Modifier.clickable { galleryLauncher.launch("image/*") },
+                      imageVector = Icons.Outlined.Edit,
+                      contentDescription = "Edit Profile Picture",
+                      tint = colorResource(R.color.black),
+                  )
+
+                  Spacer(modifier = Modifier.height(16.dp))
+
+                  Icon(
+                      modifier =
+                          Modifier.clickable {
+                            userViewModel.updateProfilePictureUrl(currentUserId, null)
+                            selectedImageUrl = null
+                          },
+                      imageVector = Icons.Outlined.Delete,
+                      contentDescription = "Delete Profile Picture",
+                      tint = colorResource(R.color.black),
+                  )
+                }
+
                 Spacer(modifier = Modifier.width(8.dp))
-                ProfilePicture(null)
+                UserProfilePicture(selectedImageUrl)
               }
             }
 
@@ -144,13 +196,20 @@ fun SettingsScreen(
         // Cancel and Save Buttons
         Row(horizontalArrangement = Arrangement.spacedBy(16.dp)) {
           ActionButtons(
-              { newName = "" }, colorResource(R.color.dark_gray), "Cancel", Modifier.weight(1f))
+              {
+                newName = ""
+                selectedImageUrl = profilePictureUrl.value
+              },
+              colorResource(R.color.dark_gray),
+              "Cancel",
+              Modifier.weight(1f))
 
           ActionButtons(
               {
                 if (newName.isNotBlank()) {
                   userViewModel.updateUserName(currentUserId, newName)
                 }
+                userViewModel.updateProfilePictureUrl(currentUserId, selectedImageUrl)
               },
               colorResource(R.color.blue),
               "Save",
@@ -167,4 +226,41 @@ fun ActionButtons(onClickAction: () -> Unit, color: Color, text: String, modifie
       modifier = modifier) {
         Text(text = text, color = colorResource(R.color.white), fontWeight = FontWeight.Bold)
       }
+}
+
+@Composable
+fun UserProfilePicture(profilePictureUri: String?) {
+  if (profilePictureUri != null) {
+    AsyncImage(
+        model = Uri.parse(profilePictureUri),
+        contentDescription = "Profile Picture",
+        modifier =
+            Modifier.size(120.dp)
+                .clip(CircleShape)
+                .background(colorResource(R.color.dark_gray))
+                .testTag("profilePictureTag"),
+        contentScale = ContentScale.Crop) // Crop the image to fit within the bounds
+  } else {
+    // Default placeholder for no image
+    Icon(
+        imageVector = Icons.Default.Person,
+        contentDescription = "Default Profile Picture",
+        modifier = Modifier.size(120.dp).testTag("default_profile_picture"))
+  }
+}
+
+fun uriToFile(context: Context, uri: Uri): File? {
+  val contentResolver = context.contentResolver
+  val file = File(context.cacheDir, "temp_image_${System.currentTimeMillis()}.jpg")
+
+  try {
+    val inputStream = contentResolver.openInputStream(uri) ?: return null
+    val outputStream = FileOutputStream(file)
+
+    inputStream.use { input -> outputStream.use { output -> input.copyTo(output) } }
+    return file
+  } catch (e: Exception) {
+    e.printStackTrace()
+  }
+  return null
 }
