ff326823b7df757163eb5c6dd44682e658a9fc79
Nassim <nassim.bensalah@epfl.ch>
2024-11-10 18:09:31 +0100

    refactor(utils): simplify SquareButton() to meet SonarCloud parameter limit

--- app/src/main/java/com/github/se/signify/ui/screens/challenge/ChallengeScreen.kt
+++ app/src/main/java/com/github/se/signify/ui/screens/challenge/ChallengeScreen.kt
@@ -96,11 +96,7 @@ fun ChallengeScreen(navigationActions: NavigationActions) {
                         iconRes = R.drawable.battleicon,
                         label = "Challenge",
                         onClick = { navigationActions.navigateTo(Route.NEW_CHALLENGE) },
-                        size = 240.dp,
-                        iconSize = 160.dp,
-                        labelFontSize = 32.sp,
-                        iconTint = colorResource(R.color.dark_gray),
-                        textColor = colorResource(R.color.dark_gray),
+                        size = 240,
                         modifier = Modifier.testTag("ChallengeButton"))
 
                     // History square button
@@ -108,12 +104,7 @@ fun ChallengeScreen(navigationActions: NavigationActions) {
                         iconRes = R.drawable.historyicon,
                         label = "History",
                         onClick = { navigationActions.navigateTo(Route.CHALLENGE_HISTORY) },
-                        size = 240.dp,
-                        iconSize = 160.dp,
-                        labelFontSize = 32.sp,
-                        iconTint = colorResource(R.color.dark_gray),
-                        textColor = colorResource(R.color.dark_gray),
-                        5.dp,
+                        size = 240,
                         modifier = Modifier.testTag("HistoryButton"))
                   }
 
