68de8b11389baf60d3a1489c865af01f3418527c
ThomasMPont <thomasmpont@gmail.com>
2024-11-11 19:32:36 +0100

    Update Theme.kt to consider light or dark system theme

--- app/src/main/java/com/github/se/signify/ui/theme/Theme.kt
+++ app/src/main/java/com/github/se/signify/ui/theme/Theme.kt
@@ -1,48 +1,67 @@
 package com.github.se.signify.ui.theme
 
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.res.colorResource
-import com.github.se.signify.R
+
+private val lightColorScheme =
+    lightColorScheme(
+        primary = md_theme_light_primary,
+        onPrimary = md_theme_light_onPrimary,
+        secondary = md_theme_light_secondary,
+        onSecondary = md_theme_light_onSecondary,
+        tertiary = md_theme_light_tertiary,
+        onTertiary = md_theme_light_onTertiary,
+        background = md_theme_light_background,
+        onBackground = md_theme_light_onBackground,
+        surface = md_theme_light_surface,
+        onSurface = md_theme_light_onSurface,
+        inverseSurface = md_theme_light_inverseSurface,
+        inverseOnSurface = md_theme_light_inverseOnSurface,
+        error = md_theme_light_error,
+        onError = md_theme_light_onError,
+        errorContainer = md_theme_light_errorContainer,
+        onErrorContainer = md_theme_light_onErrorContainer,
+        outline = md_theme_light_outline,
+        outlineVariant = md_theme_light_outlineVariant,
+    )
+
+private val darkColorScheme =
+    darkColorScheme(
+        primary = md_theme_dark_primary,
+        onPrimary = md_theme_dark_onPrimary,
+        secondary = md_theme_dark_secondary,
+        onSecondary = md_theme_dark_onSecondary,
+        tertiary = md_theme_dark_tertiary,
+        onTertiary = md_theme_dark_onTertiary,
+        background = md_theme_dark_background,
+        onBackground = md_theme_dark_onBackground,
+        surface = md_theme_dark_surface,
+        onSurface = md_theme_dark_onSurface,
+        inverseSurface = md_theme_dark_inverseSurface,
+        inverseOnSurface = md_theme_dark_inverseOnSurface,
+        errorContainer = md_theme_dark_errorContainer,
+        onErrorContainer = md_theme_dark_onErrorContainer,
+        error = md_theme_dark_error,
+        onError = md_theme_dark_onError,
+        outline = md_theme_dark_outline,
+        outlineVariant = md_theme_dark_outlineVariant,
+    )
 
 @Composable
-fun SignifyTheme(
-    // For future use if we implement a dark theme
-    // darkTheme: Boolean = isSystemInDarkTheme()
+fun SignifyTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
+  // Dynamic color is available on Android 12+
+  // We could add it in the future
 
-    // Dynamic color is available on Android 12+
-    // dynamicColor: Boolean = true,
-    content: @Composable () -> Unit
-) {
-  val lightColorScheme =
-      lightColorScheme(
-          primary = colorResource(id = R.color.blue), secondary = colorResource(id = R.color.white)
-          /* Other default colors to override
-          background = Color(0xFFFFFBFE),
-          surface = Color(0xFFFFFBFE),
-          onPrimary = Color.White,
-          onSecondary = Color.White,
-          onTertiary = Color.White,
-          onBackground = Color(0xFF1C1B1F),
-          onSurface = Color(0xFF1C1B1F),
-          */
-          )
-
-  /*
-    For future use if we implement a dark theme
-    val DarkColorScheme =
-        darkColorScheme(primary = Purple80, secondary = PurpleGrey80, tertiary = Pink80)
   val colorScheme =
-      when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-          val context = LocalContext.current
-          if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-        darkTheme -> DarkColorScheme
-        else -> LightColorScheme
+      if (!darkTheme) {
+        lightColorScheme
+      } else {
+        darkColorScheme
       }
-     */
+  MaterialTheme(colorScheme = colorScheme, content = content)
 
-  MaterialTheme(colorScheme = lightColorScheme, typography = Typography, content = content)
+  MaterialTheme(colorScheme = colorScheme, content = content)
 }
