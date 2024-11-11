afa496339f2ebf49e825e4668e5a1698272ab052
Nassim <nassim.bensalah@epfl.ch>
2024-11-11 23:21:55 +0100

    fix: remove unused import in ChallengeHistoryScreen.kt

--- app/src/main/java/com/github/se/signify/ui/screens/challenge/ChallengeHistoryScreen.kt
+++ app/src/main/java/com/github/se/signify/ui/screens/challenge/ChallengeHistoryScreen.kt
@@ -1,34 +1,14 @@
 package com.github.se.signify.ui.screens.challenge
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.github.se.signify.R
 import com.github.se.signify.ui.AnnexScreenScaffold
-import com.github.se.signify.ui.BackButton
 import com.github.se.signify.ui.NotImplementedYet
 import com.github.se.signify.ui.StatisticsRow
-import com.github.se.signify.ui.UtilTextButton
 import com.github.se.signify.ui.navigation.NavigationActions
 
 @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
