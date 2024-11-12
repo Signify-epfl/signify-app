aeac164c59ff382ca708d6a2a8b3d8c2fde4b032
ThomasMPont <thomasmpont@gmail.com>
2024-11-12 14:13:05 +0100

    Format ProfileScreen.kt using ktfmt

--- app/src/main/java/com/github/se/signify/ui/screens/profile/ProfileScreen.kt
+++ app/src/main/java/com/github/se/signify/ui/screens/profile/ProfileScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
