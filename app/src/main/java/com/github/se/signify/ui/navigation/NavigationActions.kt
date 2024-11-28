06e1e407ff7e7162ef6bfa0dfd16ad2e77c9fb6c
ThomasMPont <thomasmpont@gmail.com>
2024-11-28 20:42:29 +0100

    Fix unauthenticated toast to work from any thread

--- app/src/main/java/com/github/se/signify/ui/navigation/NavigationActions.kt
+++ app/src/main/java/com/github/se/signify/ui/navigation/NavigationActions.kt
@@ -1,6 +1,8 @@
 package com.github.se.signify.ui.navigation
 
 import android.content.Context
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
@@ -65,7 +67,9 @@ open class NavigationActions(
 
   private fun onUnauthenticated() {
     // TODO: Display a popup dialog to prompt the user to log in
-    Toast.makeText(context, context.getString(R.string.unauthenticated_error), Toast.LENGTH_SHORT)
-        .show()
+    Handler(Looper.getMainLooper()).post {
+      Toast.makeText(context, context.getString(R.string.unauthenticated_error), Toast.LENGTH_SHORT)
+          .show()
+      }
   }
 }
