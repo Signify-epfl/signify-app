aee49bbe58ce17c258423b14c9beba291fa1daa7
ziyadbagh <ziyad.baghouri@epfl.ch>
2024-11-28 13:37:39 +0100

    Update the ColorTest class

--- app/src/test/java/com/github/se/signify/ui/themes/ColorTest.kt
+++ app/src/test/java/com/github/se/signify/ui/themes/ColorTest.kt
@@ -1,42 +1,6 @@
 package com.github.se.signify.ui.themes
 
-import androidx.compose.ui.graphics.Color
-import com.github.se.signify.ui.theme.md_theme_dark_background
-import com.github.se.signify.ui.theme.md_theme_dark_error
-import com.github.se.signify.ui.theme.md_theme_dark_errorContainer
-import com.github.se.signify.ui.theme.md_theme_dark_inverseOnSurface
-import com.github.se.signify.ui.theme.md_theme_dark_inverseSurface
-import com.github.se.signify.ui.theme.md_theme_dark_onBackground
-import com.github.se.signify.ui.theme.md_theme_dark_onError
-import com.github.se.signify.ui.theme.md_theme_dark_onErrorContainer
-import com.github.se.signify.ui.theme.md_theme_dark_onPrimary
-import com.github.se.signify.ui.theme.md_theme_dark_onSecondary
-import com.github.se.signify.ui.theme.md_theme_dark_onSurface
-import com.github.se.signify.ui.theme.md_theme_dark_onTertiary
-import com.github.se.signify.ui.theme.md_theme_dark_outline
-import com.github.se.signify.ui.theme.md_theme_dark_outlineVariant
-import com.github.se.signify.ui.theme.md_theme_dark_primary
-import com.github.se.signify.ui.theme.md_theme_dark_secondary
-import com.github.se.signify.ui.theme.md_theme_dark_surface
-import com.github.se.signify.ui.theme.md_theme_dark_tertiary
-import com.github.se.signify.ui.theme.md_theme_light_background
-import com.github.se.signify.ui.theme.md_theme_light_error
-import com.github.se.signify.ui.theme.md_theme_light_errorContainer
-import com.github.se.signify.ui.theme.md_theme_light_inverseOnSurface
-import com.github.se.signify.ui.theme.md_theme_light_inverseSurface
-import com.github.se.signify.ui.theme.md_theme_light_onBackground
-import com.github.se.signify.ui.theme.md_theme_light_onError
-import com.github.se.signify.ui.theme.md_theme_light_onErrorContainer
-import com.github.se.signify.ui.theme.md_theme_light_onPrimary
-import com.github.se.signify.ui.theme.md_theme_light_onSecondary
-import com.github.se.signify.ui.theme.md_theme_light_onSurface
-import com.github.se.signify.ui.theme.md_theme_light_onTertiary
-import com.github.se.signify.ui.theme.md_theme_light_outline
-import com.github.se.signify.ui.theme.md_theme_light_outlineVariant
-import com.github.se.signify.ui.theme.md_theme_light_primary
-import com.github.se.signify.ui.theme.md_theme_light_secondary
-import com.github.se.signify.ui.theme.md_theme_light_surface
-import com.github.se.signify.ui.theme.md_theme_light_tertiary
+import com.github.se.signify.ui.theme.*
 import org.junit.Assert.assertTrue
 import org.junit.Test
 
@@ -44,56 +8,92 @@ class ColorTest {
 
   private val predefinedColors =
       setOf(
-          Color(0xFF05A9FB), // blue
-          Color(0xFF000000), // black
-          Color(0xFFD32F2F), // red
-          Color(0xFF2E7D32), // green
-          Color(0xFFFFFFFF), // white
-          Color(0xFF333333), // darkGray
-          Color(0xFFE5B6B1) // lightRed
-          )
+          Primary,
+          OnPrimary,
+          PrimaryContainer,
+          OnPrimaryContainer,
+          Secondary,
+          OnSecondary,
+          SecondaryContainer,
+          OnSecondaryContainer,
+          Tertiary,
+          OnTertiary,
+          TertiaryContainer,
+          OnTertiaryContainer,
+          Error,
+          OnError,
+          ErrorContainer,
+          OnErrorContainer,
+          Background,
+          OnBackground,
+          Surface,
+          OnSurface,
+          SurfaceVariant,
+          OnSurfaceVariant,
+          Outline,
+          PrimaryDark,
+          OnPrimaryDark,
+          PrimaryContainerDark,
+          OnPrimaryContainerDark,
+          SecondaryDark,
+          OnSecondaryDark,
+          SecondaryContainerDark,
+          OnSecondaryContainerDark,
+          TertiaryDark,
+          OnTertiaryDark,
+          TertiaryContainerDark,
+          OnTertiaryContainerDark,
+          ErrorDark,
+          OnErrorDark,
+          ErrorContainerDark,
+          OnErrorContainerDark,
+          BackgroundDark,
+          OnBackgroundDark,
+          SurfaceDark,
+          OnSurfaceDark,
+          SurfaceVariantDark,
+          OnSurfaceVariantDark,
+          OutlineDark)
 
   @Test
   fun testLightThemeColors() {
-    assertTrue(predefinedColors.contains(md_theme_light_primary))
-    assertTrue(predefinedColors.contains(md_theme_light_onPrimary))
-    assertTrue(predefinedColors.contains(md_theme_light_secondary))
-    assertTrue(predefinedColors.contains(md_theme_light_onSecondary))
-    assertTrue(predefinedColors.contains(md_theme_light_tertiary))
-    assertTrue(predefinedColors.contains(md_theme_light_onTertiary))
-    assertTrue(predefinedColors.contains(md_theme_light_background))
-    assertTrue(predefinedColors.contains(md_theme_light_onBackground))
-    assertTrue(predefinedColors.contains(md_theme_light_surface))
-    assertTrue(predefinedColors.contains(md_theme_light_onSurface))
-    assertTrue(predefinedColors.contains(md_theme_light_inverseSurface))
-    assertTrue(predefinedColors.contains(md_theme_light_inverseOnSurface))
-    assertTrue(predefinedColors.contains(md_theme_light_error))
-    assertTrue(predefinedColors.contains(md_theme_light_onError))
-    assertTrue(predefinedColors.contains(md_theme_light_errorContainer))
-    assertTrue(predefinedColors.contains(md_theme_light_onErrorContainer))
-    assertTrue(predefinedColors.contains(md_theme_light_outline))
-    assertTrue(predefinedColors.contains(md_theme_light_outlineVariant))
+    assertTrue(predefinedColors.contains(Primary))
+    assertTrue(predefinedColors.contains(OnPrimary))
+    assertTrue(predefinedColors.contains(Secondary))
+    assertTrue(predefinedColors.contains(OnSecondary))
+    assertTrue(predefinedColors.contains(Tertiary))
+    assertTrue(predefinedColors.contains(OnTertiary))
+    assertTrue(predefinedColors.contains(Background))
+    assertTrue(predefinedColors.contains(OnBackground))
+    assertTrue(predefinedColors.contains(Surface))
+    assertTrue(predefinedColors.contains(OnSurface))
+    assertTrue(predefinedColors.contains(Error))
+    assertTrue(predefinedColors.contains(OnError))
+    assertTrue(predefinedColors.contains(ErrorContainer))
+    assertTrue(predefinedColors.contains(OnErrorContainer))
+    assertTrue(predefinedColors.contains(Outline))
+    assertTrue(predefinedColors.contains(SurfaceVariant))
+    assertTrue(predefinedColors.contains(OnSurfaceVariant))
   }
 
   @Test
   fun testDarkThemeColors() {
-    assertTrue(predefinedColors.contains(md_theme_dark_primary))
-    assertTrue(predefinedColors.contains(md_theme_dark_onPrimary))
-    assertTrue(predefinedColors.contains(md_theme_dark_secondary))
-    assertTrue(predefinedColors.contains(md_theme_dark_onSecondary))
-    assertTrue(predefinedColors.contains(md_theme_dark_tertiary))
-    assertTrue(predefinedColors.contains(md_theme_dark_onTertiary))
-    assertTrue(predefinedColors.contains(md_theme_dark_background))
-    assertTrue(predefinedColors.contains(md_theme_dark_onBackground))
-    assertTrue(predefinedColors.contains(md_theme_dark_surface))
-    assertTrue(predefinedColors.contains(md_theme_dark_onSurface))
-    assertTrue(predefinedColors.contains(md_theme_dark_inverseSurface))
-    assertTrue(predefinedColors.contains(md_theme_dark_inverseOnSurface))
-    assertTrue(predefinedColors.contains(md_theme_dark_error))
-    assertTrue(predefinedColors.contains(md_theme_dark_onError))
-    assertTrue(predefinedColors.contains(md_theme_dark_errorContainer))
-    assertTrue(predefinedColors.contains(md_theme_dark_onErrorContainer))
-    assertTrue(predefinedColors.contains(md_theme_dark_outline))
-    assertTrue(predefinedColors.contains(md_theme_dark_outlineVariant))
+    assertTrue(predefinedColors.contains(PrimaryDark))
+    assertTrue(predefinedColors.contains(OnPrimaryDark))
+    assertTrue(predefinedColors.contains(SecondaryDark))
+    assertTrue(predefinedColors.contains(OnSecondaryDark))
+    assertTrue(predefinedColors.contains(TertiaryDark))
+    assertTrue(predefinedColors.contains(OnTertiaryDark))
+    assertTrue(predefinedColors.contains(BackgroundDark))
+    assertTrue(predefinedColors.contains(OnBackgroundDark))
+    assertTrue(predefinedColors.contains(SurfaceDark))
+    assertTrue(predefinedColors.contains(OnSurfaceDark))
+    assertTrue(predefinedColors.contains(ErrorDark))
+    assertTrue(predefinedColors.contains(OnErrorDark))
+    assertTrue(predefinedColors.contains(ErrorContainerDark))
+    assertTrue(predefinedColors.contains(OnErrorContainerDark))
+    assertTrue(predefinedColors.contains(OutlineDark))
+    assertTrue(predefinedColors.contains(SurfaceVariantDark))
+    assertTrue(predefinedColors.contains(OnSurfaceVariantDark))
   }
 }
