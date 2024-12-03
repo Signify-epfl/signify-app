619792be2263997798aafa4594b004a4757f5ba6
ziyadbagh <ziyad.baghouri@epfl.ch>
2024-12-03 18:34:09 +0100

    Remove existing mentions for the quests firebase repo

--- app/src/main/java/com/github/se/signify/model/di/DependencyProvider.kt
+++ app/src/main/java/com/github/se/signify/model/di/DependencyProvider.kt
@@ -4,7 +4,6 @@ import com.github.se.signify.model.auth.UserSession
 import com.github.se.signify.model.challenge.ChallengeRepository
 import com.github.se.signify.model.feedback.FeedbackRepository
 import com.github.se.signify.model.hand.HandLandMarkRepository
-import com.github.se.signify.model.quest.QuestRepository
 import com.github.se.signify.model.quiz.QuizRepository
 import com.github.se.signify.model.stats.StatsRepository
 import com.github.se.signify.model.user.UserRepository
@@ -13,8 +12,6 @@ interface DependencyProvider {
   fun challengeRepository(): ChallengeRepository
 
   fun handLandMarkRepository(): HandLandMarkRepository
-
-  fun questRepository(): QuestRepository
 
   fun statsRepository(): StatsRepository
 
--- app/src/test/java/com/github/se/signify/model/di/MockDependencyProvider.kt
+++ app/src/test/java/com/github/se/signify/model/di/MockDependencyProvider.kt
@@ -5,7 +5,6 @@ import com.github.se.signify.model.auth.UserSession
 import com.github.se.signify.model.challenge.ChallengeRepository
 import com.github.se.signify.model.feedback.FeedbackRepository
 import com.github.se.signify.model.hand.HandLandMarkRepository
-import com.github.se.signify.model.quest.QuestRepository
 import com.github.se.signify.model.quiz.QuizRepository
 import com.github.se.signify.model.stats.StatsRepository
 import com.github.se.signify.model.user.UserRepository
@@ -18,10 +17,6 @@ object MockDependencyProvider : DependencyProvider {
 
   override fun handLandMarkRepository(): HandLandMarkRepository {
     return mock(HandLandMarkRepository::class.java)
-  }
-
-  override fun questRepository(): QuestRepository {
-    return mock(QuestRepository::class.java)
   }
 
   override fun statsRepository(): StatsRepository {
