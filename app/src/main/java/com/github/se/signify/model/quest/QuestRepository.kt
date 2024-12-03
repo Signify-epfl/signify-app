221b67df3f67767c050e8eb9edefcdfdebf6eb7a
ziyadbagh <ziyad.baghouri@epfl.ch>
2024-12-03 19:34:07 +0100

    Update the quest repository

--- app/src/main/java/com/github/se/signify/model/quest/QuestRepository.kt
+++ app/src/main/java/com/github/se/signify/model/quest/QuestRepository.kt
@@ -1,8 +1,5 @@
 package com.github.se.signify.model.quest
 
 interface QuestRepository {
-
-  fun init(onSuccess: () -> Unit)
-
-  fun getDailyQuest(onSuccess: (List<Quest>) -> Unit, onFailure: (Exception) -> Unit)
+  fun getQuests(onSuccess: (List<Quest>) -> Unit, onFailure: (Exception) -> Unit)
 }
