ea0a8ad579c08b1ae44beae439f3e43acf12af6e
ziyadbagh <ziyad.baghouri@epfl.ch>
2024-12-03 18:24:44 +0100

    Remove existing tests for the quests

--- app/src/test/java/com/github/se/signify/model/quest/QuestViewModelTest.kt
+++ app/src/test/java/com/github/se/signify/model/quest/QuestViewModelTest.kt
@@ -1,25 +1,3 @@
 package com.github.se.signify.model.quest
 
-import org.junit.Before
-import org.junit.Test
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.verify
-import org.mockito.kotlin.any
-
-class QuestViewModelTest {
-
-  private lateinit var questRepository: QuestRepository
-  private lateinit var questViewModel: QuestViewModel
-
-  @Before
-  fun setUp() {
-    questRepository = mock(QuestRepository::class.java)
-    questViewModel = QuestViewModel(questRepository)
-  }
-
-  @Test
-  fun getDailyQuestCallsRepository() {
-    questViewModel.getDailyQuest()
-    verify(questRepository).getDailyQuest(any(), any())
-  }
-}
+class QuestViewModelTest {}
