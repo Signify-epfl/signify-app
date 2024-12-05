5a26778c59965aab3682e538688a7271373e4d99
ziyadbagh <ziyad.baghouri@epfl.ch>
2024-12-06 00:26:34 +0100

    Restore the quest view model tests

--- app/src/main/java/com/github/se/signify/model/quest/QuestViewModel.kt
+++ app/src/main/java/com/github/se/signify/model/quest/QuestViewModel.kt
@@ -2,30 +2,20 @@ package com.github.se.signify.model.quest
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.launch
 
-class QuestViewModel(
+open class QuestViewModel(
     private val repository: QuestRepository,
 ) : ViewModel() {
 
+  // For now fetch all the quests
   private val quest_ = MutableStateFlow<List<Quest>>(emptyList())
   val quest: StateFlow<List<Quest>> = quest_.asStateFlow()
 
   init {
-    fetchAllQuests()
-  }
-
-  fun fetchAllQuests() {
-    CoroutineScope(Dispatchers.IO).launch {
-      repository.getQuests(
-          onSuccess = { quests -> quest_.value = quests },
-          onFailure = { exception -> exception.printStackTrace() })
-    }
+    repository.init { getDailyQuest() }
   }
 
   companion object {
@@ -37,5 +27,9 @@ class QuestViewModel(
         }
       }
     }
+  }
+
+  fun getDailyQuest() {
+    repository.getDailyQuest(onSuccess = { quest_.value = it }, onFailure = {})
   }
 }
