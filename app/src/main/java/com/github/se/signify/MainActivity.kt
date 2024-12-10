549dfe484c61c92267f8ddb1d8ece9f15800e640
ziyadbagh <ziyad.baghouri@epfl.ch>
2024-12-10 20:33:38 +0100

    Update the quest screen with a new practice dialog

--- app/src/main/java/com/github/se/signify/MainActivity.kt
+++ app/src/main/java/com/github/se/signify/MainActivity.kt
@@ -243,7 +243,9 @@ fun SignifyAppPreview(
             navigationActions,
             dependencyProvider.userSession(),
             dependencyProvider.questRepository(),
-            dependencyProvider.userRepository())
+            dependencyProvider.userRepository(),
+            handLandMarkViewModel = handLandMarkViewModel,
+        )
       }
       composable(Screen.QUIZ.route) {
         QuizScreen(
--- app/src/main/java/com/github/se/signify/ui/screens/home/QuestScreen.kt
+++ app/src/main/java/com/github/se/signify/ui/screens/home/QuestScreen.kt
@@ -1,9 +1,13 @@
 package com.github.se.signify.ui.screens.home
 
+import android.util.Log
+import android.widget.Toast
 import android.widget.VideoView
 import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -24,13 +28,17 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -39,6 +47,7 @@ import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.github.se.signify.R
 import com.github.se.signify.model.auth.UserSession
+import com.github.se.signify.model.hand.HandLandMarkViewModel
 import com.github.se.signify.model.navigation.NavigationActions
 import com.github.se.signify.model.quest.Quest
 import com.github.se.signify.model.quest.QuestRepository
@@ -46,6 +55,7 @@ import com.github.se.signify.model.quest.QuestViewModel
 import com.github.se.signify.model.user.UserRepository
 import com.github.se.signify.model.user.UserViewModel
 import com.github.se.signify.ui.common.AnnexScreenScaffold
+import com.github.se.signify.ui.common.CameraBox
 
 @Composable
 fun QuestScreen(
@@ -53,7 +63,9 @@ fun QuestScreen(
     userSession: UserSession,
     questRepository: QuestRepository,
     userRepository: UserRepository,
+    handLandMarkViewModel: HandLandMarkViewModel
 ) {
+
   val questViewModel: QuestViewModel = viewModel(factory = QuestViewModel.factory(questRepository))
   val userViewModel: UserViewModel =
       viewModel(factory = UserViewModel.factory(userSession, userRepository))
@@ -69,7 +81,8 @@ fun QuestScreen(
       item { QuestTitle() }
       items(quests.value.size) { index ->
         val isUnlocked = index < unlockedQuests.toInt()
-        QuestBox(quest = quests.value[index], isUnlocked)
+        QuestBox(
+            quest = quests.value[index], isUnlocked, handLandMarkViewModel = handLandMarkViewModel)
       }
     }
   }
@@ -77,7 +90,7 @@ fun QuestScreen(
 
 @Composable
 @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-fun QuestBox(quest: Quest, isUnlocked: Boolean) {
+fun QuestBox(quest: Quest, isUnlocked: Boolean, handLandMarkViewModel: HandLandMarkViewModel) {
   var isDialogVisible by remember { mutableStateOf(false) }
   Card(
       modifier = Modifier.fillMaxWidth().padding(16.dp).testTag("QuestCard"),
@@ -113,62 +126,95 @@ fun QuestBox(quest: Quest, isUnlocked: Boolean) {
       }
   // Display the dialog if the state is true
   if (isDialogVisible) {
-    QuestDescriptionDialog(quest = quest, onDismiss = { isDialogVisible = false })
+    QuestDescriptionDialog(
+        quest = quest,
+        onDismiss = { isDialogVisible = false },
+        handLandMarkViewModel = handLandMarkViewModel)
   }
 }
 
 @Composable
 @VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
-fun QuestDescriptionDialog(quest: Quest, onDismiss: () -> Unit) {
+fun QuestDescriptionDialog(
+    quest: Quest,
+    onDismiss: () -> Unit,
+    handLandMarkViewModel: HandLandMarkViewModel
+) {
   val inSignLanguageText = stringResource(R.string.in_sign_language_text)
-  AlertDialog(
-      onDismissRequest = { onDismiss() },
-      containerColor = MaterialTheme.colorScheme.background,
-      title = {
-        Text(
-            text = "${quest.title} $inSignLanguageText",
-            fontWeight = FontWeight.Bold,
-            fontSize = 20.sp,
-            color = MaterialTheme.colorScheme.primary)
-      },
-      text = {
-        Column(modifier = Modifier.wrapContentSize().padding(8.dp)) {
-          // Display the video using VideoView
-          AndroidView(
-              factory = { context ->
-                VideoView(context).apply {
-                  setVideoPath(quest.videoPath) // Set video path (local or remote)
+  var isFingerspellVisible by remember { mutableStateOf(false) }
 
-                  // Prepare the video and start playback
-                  setOnPreparedListener { mediaPlayer ->
-                    mediaPlayer.isLooping = true // Loop video automatically
-                    start()
-                  }
-                }
-              },
-              modifier = Modifier.fillMaxWidth().height(200.dp).align(Alignment.CenterHorizontally))
-
-          Spacer(modifier = Modifier.height(20.dp))
-
+  if (isFingerspellVisible) {
+    FingerSpellDialog(
+        word = quest.title,
+        onDismiss = { isFingerspellVisible = false },
+        handLandMarkViewModel = handLandMarkViewModel)
+  } else {
+    AlertDialog(
+        onDismissRequest = { onDismiss() },
+        containerColor = MaterialTheme.colorScheme.background,
+        title = {
           Text(
-              text = quest.description, fontSize = 16.sp, color = MaterialTheme.colorScheme.primary)
-        }
-      },
-      confirmButton = {
-        Button(
-            onClick = onDismiss,
-            colors =
-                ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.primary,
-                    contentColor = MaterialTheme.colorScheme.onPrimary)) {
-              Text(stringResource(R.string.close_text))
-            }
-      },
-      shape = RoundedCornerShape(16.dp),
-      modifier = Modifier.padding(16.dp))
+              text = "${quest.title} $inSignLanguageText",
+              fontWeight = FontWeight.Bold,
+              fontSize = 20.sp,
+              color = MaterialTheme.colorScheme.primary)
+        },
+        text = {
+          Column(modifier = Modifier.wrapContentSize().padding(8.dp)) {
+            // Display the video using VideoView
+            AndroidView(
+                factory = { context ->
+                  VideoView(context).apply {
+                    setVideoPath(quest.videoPath) // Set video path (local or remote)
+
+                    // Prepare the video and start playback
+                    setOnPreparedListener { mediaPlayer ->
+                      mediaPlayer.isLooping = true // Loop video automatically
+                      start()
+                    }
+                  }
+                },
+                modifier =
+                    Modifier.fillMaxWidth().height(200.dp).align(Alignment.CenterHorizontally))
+
+            Spacer(modifier = Modifier.height(20.dp))
+
+            Text(
+                text = quest.description,
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.primary)
+          }
+        },
+        confirmButton = {
+          Row(
+              modifier = Modifier.fillMaxWidth(),
+              horizontalArrangement = Arrangement.SpaceBetween) {
+                Button(
+                    onClick = { isFingerspellVisible = true },
+                    colors =
+                        ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.secondary,
+                            contentColor = MaterialTheme.colorScheme.onSecondary)) {
+                      Text(stringResource(R.string.try_fingerspell_button_text))
+                    }
+
+                Button(
+                    onClick = onDismiss,
+                    colors =
+                        ButtonDefaults.buttonColors(
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            contentColor = MaterialTheme.colorScheme.onPrimary)) {
+                      Text(stringResource(R.string.close_text))
+                    }
+              }
+        },
+        shape = RoundedCornerShape(16.dp),
+        modifier = Modifier.padding(16.dp))
+  }
 }
 
 @Composable
+@VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
 fun QuestTitle() {
   Row(
       verticalAlignment = Alignment.CenterVertically,
@@ -183,4 +229,115 @@ fun QuestTitle() {
             fontSize = 25.sp,
             color = MaterialTheme.colorScheme.primary)
       }
+}
+
+@Composable
+@VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+fun FingerSpellDialog(
+    word: String,
+    onDismiss: () -> Unit,
+    handLandMarkViewModel: HandLandMarkViewModel
+) {
+  val context = LocalContext.current
+  var currentLetterIndex by rememberSaveable { mutableIntStateOf(0) }
+  val landmarksState = handLandMarkViewModel.landMarks().collectAsState()
+  val detectedGesture = handLandMarkViewModel.getSolution()
+
+  if (!landmarksState.value.isNullOrEmpty()) {
+    // Gesture matching logic
+    handleGestureMatchingForWord(
+        detectedGesture = detectedGesture,
+        currentLetterIndex = currentLetterIndex,
+        word = word,
+        onProgressUpdate = { newIndex -> currentLetterIndex = newIndex },
+        onWordComplete = {
+          Toast.makeText(context, "Word completed!", Toast.LENGTH_SHORT).show()
+          onDismiss() // Close dialog when word is completed
+        })
+  }
+
+  AlertDialog(
+      onDismissRequest = { onDismiss() },
+      containerColor = MaterialTheme.colorScheme.background,
+      title = {
+        Text(
+            text = stringResource(R.string.fingerspell_title_text, word),
+            fontWeight = FontWeight.Bold,
+            fontSize = 20.sp,
+            color = MaterialTheme.colorScheme.primary)
+      },
+      text = {
+        Column(modifier = Modifier.fillMaxWidth().padding(16.dp)) {
+          Text(
+              text = stringResource(R.string.try_fingerspell_text, word),
+              fontSize = 16.sp,
+              color = MaterialTheme.colorScheme.onBackground,
+              modifier = Modifier.padding(bottom = 16.dp))
+
+          WordLayer(word = word, currentLetterIndex = currentLetterIndex)
+
+          // Camera view to detect gestures
+          CameraBox(handLandMarkViewModel)
+        }
+      },
+      confirmButton = {
+        Button(
+            onClick = onDismiss,
+            colors =
+                ButtonDefaults.buttonColors(
+                    containerColor = MaterialTheme.colorScheme.primary,
+                    contentColor = MaterialTheme.colorScheme.onPrimary)) {
+              Text(stringResource(R.string.close_text))
+            }
+      })
+}
+
+@Composable
+@VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+fun WordLayer(
+    word: String,
+    currentLetterIndex: Int,
+) {
+  Box(
+      modifier =
+          Modifier.fillMaxWidth()
+              .height(150.dp)
+              .padding(horizontal = 16.dp)
+              .background(MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(16.dp))
+              .border(2.dp, MaterialTheme.colorScheme.primary, shape = RoundedCornerShape(16.dp))
+              .testTag("sentenceLayer"),
+      contentAlignment = Alignment.Center) {
+        Text(
+            text = buildForegroundText(word, currentLetterIndex),
+            modifier = Modifier.testTag("CurrentWordTag"),
+            style = TextStyle(fontSize = 30.sp),
+            color = MaterialTheme.colorScheme.onSecondary)
+      }
+}
+
+@VisibleForTesting(otherwise = VisibleForTesting.PRIVATE)
+fun handleGestureMatchingForWord(
+    detectedGesture: String,
+    currentLetterIndex: Int,
+    word: String,
+    onProgressUpdate: (newLetterIndex: Int) -> Unit,
+    onWordComplete: () -> Unit
+) {
+  if (currentLetterIndex >= word.length) {
+    return
+  }
+
+  val currentLetter = word[currentLetterIndex].toString().uppercase()
+
+  if (detectedGesture == currentLetter) {
+    if (currentLetterIndex == word.length - 1) {
+      onWordComplete()
+    } else {
+      onProgressUpdate(currentLetterIndex + 1)
+    }
+  } else {
+    Log.d(
+        "GestureMatching",
+        "Detected gesture ($detectedGesture) does not match the current letter ($currentLetter)")
+  }
 }
