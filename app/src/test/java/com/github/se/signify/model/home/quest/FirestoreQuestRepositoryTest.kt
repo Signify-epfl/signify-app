5d1492134ab8002d548afb45bf119fb1f1799280
ziyadbagh <ziyad.baghouri@epfl.ch>
2024-12-11 01:14:44 +0100

    Fix unit tests related to quests

--- app/src/test/java/com/github/se/signify/model/home/quest/FirestoreQuestRepositoryTest.kt
+++ app/src/test/java/com/github/se/signify/model/home/quest/FirestoreQuestRepositoryTest.kt
@@ -289,7 +289,7 @@ class FirestoreQuestRepositoryTest {
   @Test
   fun fetchVideo_constructsCorrectPath() {
     // construct video path
-    val videoPath = questRepositoryFirestore.fetchVideo("Thank You")
+    val videoPath = firestoreQuestRepository.fetchVideo("Thank You")
 
     // Assert
     assertEquals("android.resource://com.github.se.signify/raw/thankyou", videoPath)
