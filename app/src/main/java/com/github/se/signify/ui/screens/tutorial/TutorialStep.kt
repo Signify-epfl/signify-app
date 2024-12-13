99d16f1828fec611baf6d09f159785b6f752d3fe
nada_ezziti <nada.ezziti@epfl.ch>
2024-12-13 02:00:30 +0100

    Update the implementation of the TutorialScreen

---
+++ app/src/main/java/com/github/se/signify/ui/screens/tutorial/TutorialStep.kt
@@ -0,0 +1,4 @@
+package com.github.se.signify.ui.screens.tutorial
+
+// Data class to represent a single tutorial step
+data class TutorialStep(val text: String, val highlightArea: HighlightedArea, val textTag: String)
