11f78ba0b5f06142ce450faacc0734260995656b
ziyadbagh <ziyad.baghouri@epfl.ch>
2024-12-17 07:38:53 +0100

    Updated welcome screen UI test class

--- app/src/androidTest/java/com/github/se/signify/ui/screens/welcome/WelcomeScreenTest.kt
+++ app/src/androidTest/java/com/github/se/signify/ui/screens/welcome/WelcomeScreenTest.kt
@@ -4,6 +4,8 @@ import androidx.compose.ui.test.assertIsDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import com.github.se.signify.model.authentication.MockUserSession
+import com.github.se.signify.model.authentication.UserSession
 import com.github.se.signify.model.navigation.NavigationActions
 import org.junit.Before
 import org.junit.Rule
@@ -15,16 +17,18 @@ class WelcomeScreenTest {
   @get:Rule val composeTestRule = createComposeRule()
 
   private lateinit var navigationActions: NavigationActions
+  private lateinit var userSession: UserSession
 
   @Before
   fun setUp() {
     navigationActions = Mockito.mock(NavigationActions::class.java)
+    userSession = MockUserSession()
   }
 
   @Test
   fun welcomeScreenDisplaysCorrectInitialImageAndText() {
     // Set the content of the WelcomeScreen
-    composeTestRule.setContent { WelcomeScreen(navigationActions) }
+    composeTestRule.setContent { WelcomeScreen(navigationActions, userSession) }
 
     // Verify that the first image (letter_s) is displayed
     composeTestRule.onNodeWithContentDescription("Hand Sign Images").assertIsDisplayed()
